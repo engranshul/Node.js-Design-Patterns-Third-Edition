018caacd1f794893c07129b26e2ee1a279315115
Anshul Chauhan <anshul101289@gmail.com>
2022-09-01 08:44:39 +0530

    node mania

--- 02-the-module-system/01-revealing-module-pattern/index.js
+++ 02-the-module-system/01-revealing-module-pattern/index.js
@@ -15,10 +15,17 @@ console.log(myModule)
 console.log(myModule.privateFoo, myModule.privateBar)
 
 
-// CommonJS defines a module format. Unfortunately, it was defined without 
-// giving browsers equal footing to other JavaScript environments
+// Most successful JS module systems are : asynchronous module definition(amd)
+// popularized by RequireJs and later universal module defintion(umd)
 
-// RequireJS tries to keep with the spirit of CommonJS, with using string 
-// names to refer to dependencies, and to avoid modules defining global 
-// objects, but still allow coding a module format that works well natively 
-// in the browser
+// Node.js has two module systems: CommonJS modules and ECMAScript modules.
+
+// Feeling is that ECMAScript modules is going to become the defacto way to
+// manage javascript modules in both browser n server landscape
+
+// JavaScript programs started off pretty small — most of its usage in the
+// early days was to do isolated scripting tasks, providing a bit of 
+// interactivity to your web pages where needed, so large scripts were
+// generally not needed. Fast forward a few years and we now have complete 
+// applications being run in browsers with a lot of JavaScript, as well as 
+// JavaScript being used in other contexts (Node.js, for example).
